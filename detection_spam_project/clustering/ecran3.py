a864a9dc5f83d7b1b2a26b2a51aaec67f9da69c6
Karakayn <karakayn@darluok-server.com>
2016-04-19 17:08:38 +0200

    add notes for marc2

--- detection_spam_project/clustering/ecran3.py
+++ detection_spam_project/clustering/ecran3.py
@@ -6,4 +6,5 @@ def view_screen(request):
 	#pour marc : http://scikit-learn.org/stable/auto_examples/cluster/plot_mini_batch_kmeans.html#example-cluster-plot-mini-batch-kmeans-py
 	#ca pourra t'inspirer
 	#oublie pas dutiliser D3JS pour l'affichage des graphs ! 
+	#on doit faire notre propre biblio Kmeans comme en TP mais ca peut t'inspirer
 	return render(request, 'ecran3.html', {'id_screen': 3})
