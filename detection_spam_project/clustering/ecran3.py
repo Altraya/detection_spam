9a9d67ab66f8247b805a779d41e0cd32215e6c98
Karakayn <karakayn@darluok-server.com>
2016-04-19 17:04:46 +0200

    add notes for marc

--- detection_spam_project/clustering/ecran3.py
+++ detection_spam_project/clustering/ecran3.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 def view_screen(request):
-
+	#pour marc : http://scikit-learn.org/stable/auto_examples/cluster/plot_mini_batch_kmeans.html#example-cluster-plot-mini-batch-kmeans-py
+	#ca pourra t'inspirer
+	#oublie pas dutiliser D3JS pour l'affichage des graphs ! 
 	return render(request, 'ecran3.html', {'id_screen': 3})
