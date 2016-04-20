28028e5dc2bceba7daeb17f2157a5e95756b3cb7
Karakayn <karakayn@darluok-server.com>
2016-04-20 12:57:57 +0200

    Commit avant de partir sur le load du fichier data

--- detection_spam_project/clustering/ecran2.py
+++ detection_spam_project/clustering/ecran2.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
 from clustering.form import ScreenOneForm
 import ecran1
+import csv
+import re
 
 def view_screen(request):
 
@@ -18,8 +20,6 @@ def view_screen(request):
 			#ici on peut travailler sur notre fichier
 			#idColumns = []
 			idColumns = request.POST.getlist('champsClassification')
-			print "idColumns : "
-			print idColumns
 			#normalisation
 			resulNormalize = normalizeMe(idColumns, fichier)
 
@@ -33,10 +33,62 @@ def view_screen(request):
 		return render(request, 'ecran1.html', {'id_screen': 1}) 
 
 """
-	normalize les champs dont le numero de colonne est donné 
-	en paramètre dans le fichier donné en parametre et 
-	retourne un tableau contenant les tableaux de resultats
+	Fonction de normalisation de champs
+	@params : tableaux multidimentionnel contenant les ids des champs de classification a normaliser
+	@fichier : le fichier dans lequel se trouve les champs a normaliser
+	@return : tableau contenant les tableaux resultats des champs normalisés
 """
 def normalizeMe(params, fichier):
 
+	# [ 3.72 ] [1] 
+	print params
+
+	currentLine = []
+	matrix = []
+	data = []
+	test = []
+	#ouverture fichier
+	for line in fichier.readlines():
+		currentLine = line.split(",")
+		#on doit rstrip toutes les infos de cette liste pour ne pas avoir de \n
+		#il faut donc la parcourrir et les enlever
+		for number in currentLine:
+			number.rstrip()
+			matrix.append(number)
+	#test = data.readlines()
+	#print number
+	#print matrix
+
+
+	
+	#i = 0 #index les lignes vont jusqu'a 57
+	#line = data.split(",")
+
+
+
+	print "matrix = " + str(matrix)
+	#for eachNumber in line:
+		#print line
+		#print eachNumber
+		#if "\n" in line:
+			#print "\n trouvé"
+			#gaucheEtDroite = line.split('\n')
+			#print gaucheEtDroite
+	"""reader = csv.reader(data)
+
+	for row in reader:
+		print row
+			#row[i] = (float(row[i]) - minArr[i])
+
+	print yeah"""
+
+	#print data
+	#print ""
+	#print line
+
+	#cherche toutes les colonnes correspondantes et crée un tableau a la volée contenant les valeurs de chaque colonne
+
+	#normalise chaque colonne
+
+	#retourne le resultat
 	return "oui"
